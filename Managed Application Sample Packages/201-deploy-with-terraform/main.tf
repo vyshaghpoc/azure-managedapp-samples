b85925ea26bfe25b76eabdc93ec291accc6c70d4
Vyshagh <pocvyshagh@gmail.com>
2023-02-05 09:08:13 +0100

    testing

--- Managed Application Sample Packages/201-deploy-with-terraform/main.tf
+++ Managed Application Sample Packages/201-deploy-with-terraform/main.tf
@@ -1,7 +1,7 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
     version         = "=2.0.0"
-    subscription_id = var.azure_subscription_id
+    #subscription_id = var.azure_subscription_id
     #client_id       = var.service_principal_id
     #client_secret   = var.service_principal_secret
     #tenant_id       = var.azure_ad_tenant_id 
